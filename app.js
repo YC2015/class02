57b11badc9c48439face694a1405c27e86b91591
YC <yamile.colque@gmail.com>
2016-05-25 21:06:29 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// var userName = prompt('What\'s your name');
+// alert('Welcome to my page, ' + userName);
+
+
+var questionOne = prompt ('What state was I born in?').toLowerCase();
+//questionOne.toLowerCase();
+//string method
+
+if (questionOne === 'washington') {
+    alert ('Yay, you know me so well! \n Gold Star!');
+} else {
+    alert ('Sorry, you no know me ... Sad Trombone.');  
+}
